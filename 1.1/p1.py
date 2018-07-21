4e1deeed6b666729119f29d1bfc290faf4980189
Francesco Mannella <francesco.mannella@gmail.com>
2018-07-21 11:18:33 +0200

    correct comments to p111

--- 1.1/p1.py
+++ 1.1/p1.py
@@ -3,15 +3,12 @@
 import numpy as np
 
 
-# Suppose A ∈ ℝ^(n × n) and x ∈ ℝ^(r) are given. Give an algorithm for computing
-# the first column of M = (A − x₁I)...(A − xrI)  
+# Suppose A ∈ ℝ^(n × n) and x ∈ ℝ^(r) are given. Give an algorithm for
+# computing the first column of M = (A − x₁I)...(A − xrI)  
 
-# Solution: A single column cannot be computed indipendently from the rest of
-# the matrix. This operation cannot be decompose into indipendent operations
-# over columns
-
-
-
+# Solution: A single column cannot be computed indipendently from the
+# rest of the matrix. This operation cannot be decomposed into
+# indipendent operations over columns
 
 n = 4
 r = 3
@@ -21,24 +18,20 @@ A = np.arange(n*n).reshape(n, n)
 x = np.arange(r)
 I = np.eye(n, n)
 
-
 # Compute by columns
-# We must comute all columns at each iteration because they are all neded in the
-# multiplication
-
 # store te curren state of the multiplication over iterations 
 D = np.zeros((n, n))   
 # store last multiplication
 B = I.copy()  
-
 C = 0*I
-B = I.copy()
+
 # iterate over x elements
 for k in range(r):   
     # the matrix to be multiplied with the current result of 
     # previous iterations
     E = A - x[k] * I 
-    # storage for the current multiplication
+    # We must comute all columns at each iteration because they are
+    # all neded in the multiplication
     for j in range(n):
         C[:, j]  += np.dot(B, E[:, j])
     B = C.copy()
@@ -46,8 +39,9 @@ for k in range(r):
 
 print
 
-# Compute by rows
+# Instead it can be decomposed into indpendent operations over rows
 
+# Compute by rows
 # initial value of the desired row 
 b = I[0]
 # store desired row over iterations
@@ -58,7 +52,8 @@ for k in range(r):
     # the matrix to be multiplied with the current result of 
     # previous iterations
     E = A - x[k] * I 
-    # storage for the current multiplication
+    # we compute only the multiplication between the desired column
+    # and the k-th matrix
     c  += np.dot(b, E)
     b = c.copy()
 
